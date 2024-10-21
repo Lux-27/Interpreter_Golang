c2efb88beb653a8af3b288d501cb6470bfd61a86
Luxyy <raj170303@gmail.com>
2024-10-22 00:59:52 +0530

    Parser Completed

---
+++ go.mod
@@ -0,0 +1,3 @@
+module InterpreterGolang
+
+go 1.22.6
---
+++ repl/repl.go
@@ -0,0 +1,48 @@
+package repl
+
+import (
+	"InterpreterGolang/lexer"
+	"InterpreterGolang/parser"
+	"bufio"
+	"fmt"
+	"io"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+const MONKEY_FACE = ` Arcadium is ....dyinnngg---dead
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! Something went poof!!\n")
+	io.WriteString(out, "parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
